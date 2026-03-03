4711bae3571fcf9fd2c05c82257d29c0eb19c681
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2026-03-03 16:47:56 -0300

    feat: add initial files

---
+++ src/test/java/com/eventshub/EventsHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventshub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
