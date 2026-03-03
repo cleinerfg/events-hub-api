a71698d5308c699983321ca0efcfcfa1dd68b765
CleinerFG <149598883+CleinerFG@users.noreply.github.com>
2026-03-03 16:47:56 -0300

    feat: add initial files

---
+++ src/main/java/com/eventshub/EventsHubApplication.java
@@ -0,0 +1,13 @@
+package com.eventshub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventsHubApplication.class, args);
+    }
+
+}
