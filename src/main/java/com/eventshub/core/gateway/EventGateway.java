793046aaf86653ea6c8e7b3ce25658f42a0a7d54
cleinerfg <https://cleinerfg@github.com>
2026-03-09 16:15:36 -0300

    feat(event): add findAll at EventGateway

--- src/main/java/com/eventshub/core/gateway/EventGateway.java
+++ src/main/java/com/eventshub/core/gateway/EventGateway.java
@@ -2,7 +2,11 @@ package com.eventshub.core.gateway;
 
 import com.eventshub.core.domain.Event;
 
+import java.util.List;
+
 public interface EventGateway {
 
     Event create(Event event);
+
+    List<Event> findAll();
 }
