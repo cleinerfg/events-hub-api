fbd85ca757ab1901e4e84282c98884b4c89634ea
cleinerfg <149598883+cleinerfg@users.noreply.github.com>
2026-03-09 01:52:26 -0300

    feat(event): create EventDomainMapper for domain and persistence conversion

---
+++ src/main/java/com/eventshub/infra/mapper/EventDomainMapper.java
@@ -0,0 +1,37 @@
+package com.eventshub.infra.mapper;
+
+import com.eventshub.core.domain.Event;
+import com.eventshub.infra.persistence.EventEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EventDomainMapper {
+
+    public EventEntity toEntity(Event event) {
+        return EventEntity.builder()
+                .id(event.id())
+                .identifier(event.identifier())
+                .name(event.name())
+                .type(event.type())
+                .description(event.description())
+                .organizer(event.organizer())
+                .location(event.location())
+                .startDate(event.startDate())
+                .endDate(event.endDate())
+                .build();
+    }
+
+    public Event toDomain(EventEntity eventEntity) {
+        return Event.builder()
+                .id(eventEntity.getId())
+                .identifier(eventEntity.getIdentifier())
+                .name(eventEntity.getName())
+                .type(eventEntity.getType())
+                .description(eventEntity.getDescription())
+                .organizer(eventEntity.getOrganizer())
+                .location(eventEntity.getLocation())
+                .startDate(eventEntity.getStartDate())
+                .endDate(eventEntity.getEndDate())
+                .build();
+    }
+}
