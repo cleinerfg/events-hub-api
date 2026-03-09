1925f311fa0db5788765426d6c6e661020ba7b68
cleinerfg <https://cleinerfg@github.com>
2026-03-09 16:17:16 -0300

    feat(event): add bean configuration in EventConfig for FindAllEventsUseCase

--- src/main/java/com/eventshub/infra/beans/EventConfig.java
+++ src/main/java/com/eventshub/infra/beans/EventConfig.java
@@ -3,6 +3,8 @@ package com.eventshub.infra.beans;
 import com.eventshub.core.gateway.EventGateway;
 import com.eventshub.core.usecases.CreateEventUseCase;
 import com.eventshub.core.usecases.CreateEventUseCaseImpl;
+import com.eventshub.core.usecases.FindAllEventsUseCase;
+import com.eventshub.core.usecases.FindAllEventsUseCaseImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,5 +14,10 @@ public class EventConfig {
     @Bean
     public CreateEventUseCase createEventUseCase(EventGateway eventGateway) {
         return new CreateEventUseCaseImpl(eventGateway);
+    }
+
+    @Bean
+    public FindAllEventsUseCase findAllEventsUseCase(EventGateway eventGateway) {
+        return new FindAllEventsUseCaseImpl(eventGateway);
     }
 }
