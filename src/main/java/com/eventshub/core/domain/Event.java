bb2bff92b09c859eed6671c9d02e9c04f92084b2
cleinerfg <https://cleinerfg@github.com>
2026-03-09 01:37:36 -0300

    feat(event): create Event domain

---
+++ src/main/java/com/eventshub/core/domain/Event.java
@@ -0,0 +1,20 @@
+package com.eventshub.core.domain;
+
+import com.eventshub.core.enums.EventType;
+import lombok.Builder;
+
+import java.time.OffsetDateTime;
+
+@Builder
+public record Event(
+        Long id,
+        String identifier,
+        String name,
+        EventType type,
+        String description,
+        String organizer,
+        String location,
+        OffsetDateTime startDate,
+        OffsetDateTime endDate
+) {
+}
