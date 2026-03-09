2979dbed6d34933d2232fc4eb4afcfa2a5f5d2d7
cleinerfg <https://cleinerfg@github.com>
2026-03-09 18:02:27 -0300

    feat(event): add method existsByIdentifier to EventRepository

--- src/main/java/com/eventshub/infra/persistence/EventRepository.java
+++ src/main/java/com/eventshub/infra/persistence/EventRepository.java
@@ -3,4 +3,6 @@ package com.eventshub.infra.persistence;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EventRepository extends JpaRepository<EventEntity, Long> {
+
+    boolean existsByIdentifier(String identifier);
 }
