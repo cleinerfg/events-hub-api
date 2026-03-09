1c1098c05b98a64d9f0171f1e4eb4c7dc189160e
cleinerfg <https://cleinerfg@github.com>
2026-03-09 01:53:23 -0300

    feat(event): implement EventRepositoryGateway

---
+++ src/main/java/com/eventshub/infra/gateway/EventRepositoryGateway.java
@@ -0,0 +1,25 @@
+package com.eventshub.infra.gateway;
+
+import com.eventshub.core.domain.Event;
+import com.eventshub.core.gateway.EventGateway;
+import com.eventshub.infra.mapper.EventDomainMapper;
+import com.eventshub.infra.persistence.EventEntity;
+import com.eventshub.infra.persistence.EventRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@RequiredArgsConstructor
+@Repository
+public class EventRepositoryGateway implements EventGateway {
+
+    private final EventRepository eventRepository;
+    private final EventDomainMapper eventDomainMapper;
+
+    @Override
+    public Event create(Event event) {
+        EventEntity savedEvent = eventRepository.save(
+                eventDomainMapper.toEntity(event)
+        );
+        return eventDomainMapper.toDomain(savedEvent);
+    }
+}
