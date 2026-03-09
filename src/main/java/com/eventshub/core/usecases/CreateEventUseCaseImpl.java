cd2e365f8f1f448910160ce143839692dd20528e
cleinerfg <149598883+cleinerfg@users.noreply.github.com>
2026-03-09 18:04:05 -0300

    feat(event): add verify if Event identifier exists at CreateEventUseCaseImpl

--- src/main/java/com/eventshub/core/usecases/CreateEventUseCaseImpl.java
+++ src/main/java/com/eventshub/core/usecases/CreateEventUseCaseImpl.java
@@ -1,6 +1,7 @@
 package com.eventshub.core.usecases;
 
 import com.eventshub.core.domain.Event;
+import com.eventshub.core.exception.DuplicateEventIdentifierException;
 import com.eventshub.core.gateway.EventGateway;
 import lombok.RequiredArgsConstructor;
 
@@ -11,6 +12,9 @@ public class CreateEventUseCaseImpl implements CreateEventUseCase {
 
     @Override
     public Event execute(Event event) {
+        if (eventGateway.existsByIdentifier(event.identifier())) {
+            throw new DuplicateEventIdentifierException(event.identifier());
+        }
         return eventGateway.create(event);
     }
 }
