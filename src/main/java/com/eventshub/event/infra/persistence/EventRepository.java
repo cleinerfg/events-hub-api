1a81e764b0f29cf0b846a2d9d1dd8e6ce1ebc5b6
cleinerfg <149598883+cleinerfg@users.noreply.github.com>
2026-03-11 19:03:06 -0300

    feat(event): implement JpaSpecificationExecutor in EventRepository
    Now extends JpaSpecificationExecutor to enable the use of EventSpecs for dynamic filtering.

--- src/main/java/com/eventshub/event/infra/persistence/EventRepository.java
+++ src/main/java/com/eventshub/event/infra/persistence/EventRepository.java
@@ -1,10 +1,13 @@
 package com.eventshub.event.infra.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.Optional;
 
-public interface EventRepository extends JpaRepository<EventJpaEntity, Long> {
+public interface EventRepository extends
+        JpaRepository<EventJpaEntity, Long>,
+        JpaSpecificationExecutor<EventJpaEntity> {
 
     boolean existsByIdentifier(String identifier);
 
