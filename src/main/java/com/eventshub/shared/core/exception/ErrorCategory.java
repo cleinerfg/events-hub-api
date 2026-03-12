92a509fd90622d0b651ee1463cc7825420b23dbd
cleinerfg <149598883+cleinerfg@users.noreply.github.com>
2026-03-12 19:58:09 -0300

    feat(shared): add ErrorCategory enum to classify application errors

---
+++ src/main/java/com/eventshub/shared/core/exception/ErrorCategory.java
@@ -0,0 +1,9 @@
+package com.eventshub.shared.core.exception;
+
+public enum ErrorCategory {
+    BUSINESS_RULE_VIOLATION,
+    UNAUTHORIZED,
+    NOT_FOUND,
+    INVALID_INPUT,
+    INTERNAL_SERVER_ERROR,
+}
