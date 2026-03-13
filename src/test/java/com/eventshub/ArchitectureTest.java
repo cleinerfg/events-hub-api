cfdca4f65f69d588f51ac74dc7119b4f850302b1
cleinerfg <149598883+cleinerfg@users.noreply.github.com>
2026-03-12 23:04:34 -0300

    test: refactor architecture tests and add module isolation rules
    Refactors core_must_be_pure and introduces shared_must_not_depend_on_business_modules and modules_must_be_isolated_except_from_shared rules. It also standardizes package path constants using private static final strings to improve maintainability.

--- src/test/java/com/eventshub/ArchitectureTest.java
+++ src/test/java/com/eventshub/ArchitectureTest.java
@@ -5,13 +5,34 @@ import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
 
+import static com.tngtech.archunit.core.domain.JavaClass.Predicates.resideInAPackage;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
 
 @AnalyzeClasses(packages = "com.eventshub", importOptions = ImportOption.DoNotIncludeTests.class)
 public class ArchitectureTest {
 
+    private static final String MODULE_PACKAGE = "com.eventshub.modules.(*)..";
+    private static final String SHARED_PACKAGE = "com.eventshub.shared..";
+    private static final String ANY_CORE = "..core..";
+    private static final String ANY_INFRA = "..infra..";
+
     @ArchTest
-    static final ArchRule core_layer_must_be_isolated = noClasses()
-            .that().resideInAPackage("..event.core..")
-            .should().dependOnClassesThat().resideInAPackage("..event.infra..");
+    static final ArchRule core_must_be_pure = noClasses()
+            .that().resideInAPackage(ANY_CORE)
+            .should().dependOnClassesThat().resideInAPackage(ANY_INFRA)
+            .because("The core should not have access to infrastructure details");
+
+    @ArchTest
+    static final ArchRule shared_must_not_depend_on_business_modules = noClasses()
+            .that().resideInAPackage(SHARED_PACKAGE)
+            .should().dependOnClassesThat().resideInAPackage("com.eventshub.modules..")
+            .because("The shared is multifunctional and should not be subject to specific business rules");
+
+    @ArchTest
+    static final ArchRule modules_must_be_isolated_except_from_shared = slices()
+            .matching(MODULE_PACKAGE)
+            .should().notDependOnEachOther()
+            .ignoreDependency(resideInAPackage(MODULE_PACKAGE), resideInAPackage(SHARED_PACKAGE))
+            .because("Business modules should be isolated from each other, but they can use shared");
 }
