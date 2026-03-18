276f36a0a10b626baa18f7e0911fb5ac38e2d9e8
cleinerfg <149598883+cleinerfg@users.noreply.github.com>
2026-03-18 14:27:27 -0300

    feat: add migration to create event type enum in database

---
+++ src/main/resources/db/migration/V1__create_event_type_enum.sql
@@ -0,0 +1,14 @@
+CREATE TYPE event_type AS ENUM (
+    'NETWORKING',
+    'MUSIC',
+    'SPORTS',
+    'ART',
+    'TECHNOLOGY',
+    'WORKSHOP',
+    'GASTRONOMY',
+    'WELLNESS',
+    'FASHION',
+    'THEATER',
+    'E_SPORTS',
+    'OTHER'
+    );
