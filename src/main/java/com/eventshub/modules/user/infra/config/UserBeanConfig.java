1c39bbfaea37743cefed899f64af4d9deb4aa544
cleinerfg <149598883+cleinerfg@users.noreply.github.com>
2026-03-19 16:47:13 -0300

    feat(auth): add LoginUseCase bean to UserBeanConfig

--- src/main/java/com/eventshub/modules/user/infra/config/UserBeanConfig.java
+++ src/main/java/com/eventshub/modules/user/infra/config/UserBeanConfig.java
@@ -1,8 +1,11 @@
 package com.eventshub.modules.user.infra.config;
 
+import com.eventshub.modules.user.core.port.AuthPort;
 import com.eventshub.modules.user.core.port.PasswordEncoderPort;
+import com.eventshub.modules.user.core.port.TokenPort;
 import com.eventshub.modules.user.core.port.UserPort;
 import com.eventshub.modules.user.core.usecase.CreateUserUseCase;
+import com.eventshub.modules.user.core.usecase.LoginUseCase;
 import com.eventshub.shared.core.port.UuidGeneratorPort;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,5 +20,13 @@ public class UserBeanConfig {
             PasswordEncoderPort passwordEncoderPort
     ) {
         return new CreateUserUseCase(port, uuidGeneratorPort, passwordEncoderPort);
+    }
+
+    @Bean
+    public LoginUseCase loginUserUseCase(
+            AuthPort authPort,
+            TokenPort tokenPort
+    ) {
+        return new LoginUseCase(authPort, tokenPort);
     }
 }
