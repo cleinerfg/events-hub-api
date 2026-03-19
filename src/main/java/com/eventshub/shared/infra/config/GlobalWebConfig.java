a1987bbe19469cf8e6e68694f73c9dfba342f129
cleinerfg <149598883+cleinerfg@users.noreply.github.com>
2026-03-19 12:50:09 -0300

    feat(shared): define globalHttpErrorSchema bean for global error definitions

---
+++ src/main/java/com/eventshub/shared/infra/config/GlobalWebConfig.java
@@ -0,0 +1,31 @@
+package com.eventshub.shared.infra.config;
+
+import com.eventshub.shared.core.exception.AppError;
+import com.eventshub.shared.core.exception.ErrorScope;
+import com.eventshub.shared.core.exception.GlobalAppError;
+import com.eventshub.shared.infra.web.exception.HttpErrorSchema;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+
+import java.util.Map;
+
+@Configuration
+public class GlobalWebConfig {
+
+    @Bean
+    public HttpErrorSchema globalHttpErrorSchema() {
+        Map<AppError, HttpStatus> map = Map.of(
+                GlobalAppError.SYSTEM_INTEGRITY_ERROR, HttpStatus.INTERNAL_SERVER_ERROR,
+                GlobalAppError.INVALID_JSON, HttpStatus.BAD_REQUEST,
+                GlobalAppError.RESOURCE_NOT_FOUND, HttpStatus.NOT_FOUND,
+                GlobalAppError.INVALID_PERIOD, HttpStatus.BAD_REQUEST
+        );
+
+        return HttpErrorSchema.builder()
+                .scope(ErrorScope.GLOBAL)
+                .definitions(map)
+                .build();
+
+    }
+}
