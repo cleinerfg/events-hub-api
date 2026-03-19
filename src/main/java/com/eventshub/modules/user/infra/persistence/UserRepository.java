3f31eec6a09d299706aa2f563bfa38ba79859249
cleinerfg <149598883+cleinerfg@users.noreply.github.com>
2026-03-19 16:36:41 -0300

    feat(user): add findByEmail to UserRepository

--- src/main/java/com/eventshub/modules/user/infra/persistence/UserRepository.java
+++ src/main/java/com/eventshub/modules/user/infra/persistence/UserRepository.java
@@ -2,7 +2,11 @@ package com.eventshub.modules.user.infra.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserJpaEntity, Long> {
 
     boolean existsByEmail(String email);
+
+    Optional<UserJpaEntity> findByEmail(String email);
 }
