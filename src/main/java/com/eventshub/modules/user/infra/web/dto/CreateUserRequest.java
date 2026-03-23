4fb89aec5c5296f57cbc1dc6229fc07be00c570f
cleinerfg <149598883+cleinerfg@users.noreply.github.com>
2026-03-22 22:18:27 -0300

    feat(user): create CreateUserRequest with validation annotations

---
+++ src/main/java/com/eventshub/modules/user/infra/web/dto/CreateUserRequest.java
@@ -0,0 +1,22 @@
+package com.eventshub.modules.user.infra.web.dto;
+
+import com.eventshub.shared.infra.web.exception.ValidationMessage;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CreateUserRequest(
+        @NotBlank(message = ValidationMessage.REQUIRED)
+        @Size(max = 255, message = ValidationMessage.MAX_LENGTH_255)
+        String name,
+
+        @NotBlank(message = ValidationMessage.REQUIRED)
+        @Email(message = ValidationMessage.INVALID_EMAIL_FORMAT)
+        @Size(max = 255, message = ValidationMessage.MAX_LENGTH_255)
+        String email,
+
+        @NotBlank(message = ValidationMessage.REQUIRED)
+        @Size(max = 255, message = ValidationMessage.MAX_LENGTH_255)
+        String password
+) {
+}
