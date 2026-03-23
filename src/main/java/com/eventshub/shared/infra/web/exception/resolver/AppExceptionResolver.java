81be9b410f642f3c8cebe6e25c8b45df1d8e7797
cleinerfg <149598883+cleinerfg@users.noreply.github.com>
2026-03-22 22:31:31 -0300

    refactor(user): update AppExceptionResolver to handle InvalidPasswordException

--- src/main/java/com/eventshub/shared/infra/web/exception/resolver/AppExceptionResolver.java
+++ src/main/java/com/eventshub/shared/infra/web/exception/resolver/AppExceptionResolver.java
@@ -1,10 +1,14 @@
 package com.eventshub.shared.infra.web.exception.resolver;
 
+import com.eventshub.modules.user.core.exception.InvalidPasswordException;
+import com.eventshub.shared.core.exception.AppError;
 import com.eventshub.shared.core.exception.AppException;
 import com.eventshub.shared.infra.web.exception.support.ProblemDetailFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ProblemDetail;
 import org.springframework.stereotype.Component;
+
+import java.util.Set;
 
 @Component
 @RequiredArgsConstructor
@@ -13,7 +17,15 @@ public class AppExceptionResolver {
     private final ProblemDetailFactory problemDetailFactory;
 
     public ProblemDetail resolve(AppException ex) {
-        var error = ex.getError();
-        return problemDetailFactory.create(error, ex.getMessage());
+        AppError error = ex.getError();
+
+        var problemDetail = problemDetailFactory.create(error, ex.getMessage());
+
+        if (ex instanceof InvalidPasswordException invalidPasswordEx) {
+            Set<String> fails = invalidPasswordEx.getFails();
+            problemDetail.setProperty("fails", fails);
+        }
+
+        return problemDetail;
     }
 }
