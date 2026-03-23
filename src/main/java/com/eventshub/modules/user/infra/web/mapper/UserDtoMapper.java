6728ce66e52ea217e53fcec50b63604386473e8a
cleinerfg <149598883+cleinerfg@users.noreply.github.com>
2026-03-22 22:22:13 -0300

    feat(user): add toCreateInput method to UserDtoMapper

--- src/main/java/com/eventshub/modules/user/infra/web/mapper/UserDtoMapper.java
+++ src/main/java/com/eventshub/modules/user/infra/web/mapper/UserDtoMapper.java
@@ -1,7 +1,10 @@
 package com.eventshub.modules.user.infra.web.mapper;
 
 import com.eventshub.modules.user.core.model.User;
+import com.eventshub.modules.user.core.model.input.CreateUserInput;
+import com.eventshub.modules.user.infra.web.dto.CreateUserRequest;
 import com.eventshub.modules.user.infra.web.dto.UserResponse;
+import com.eventshub.shared.core.support.StringSanitizer;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,6 +15,14 @@ public class UserDtoMapper {
                 .externalId(user.getExternalId())
                 .name(user.getName())
                 .email(user.getEmail())
+                .build();
+    }
+
+    public CreateUserInput toCreateInput(CreateUserRequest request) {
+        return CreateUserInput.builder()
+                .name(StringSanitizer.capitalize(request.name()))
+                .email(StringSanitizer.trimAndClean(request.email()).toLowerCase())
+                .password(request.password())
                 .build();
     }
 }
