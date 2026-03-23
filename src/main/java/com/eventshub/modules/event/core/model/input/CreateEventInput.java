df41aed82a8a9fd726452b06a9539624605c06be
cleinerfg <149598883+cleinerfg@users.noreply.github.com>
2026-03-22 22:08:54 -0300

    refactor(event): add CheckNotNull validation to CreateEventInput

--- src/main/java/com/eventshub/modules/event/core/model/input/CreateEventInput.java
+++ src/main/java/com/eventshub/modules/event/core/model/input/CreateEventInput.java
@@ -1,9 +1,12 @@
 package com.eventshub.modules.event.core.model.input;
 
 import com.eventshub.modules.event.core.model.EventType;
+import com.eventshub.shared.core.exception.support.CheckNotNull;
+import lombok.Builder;
 
 import java.time.OffsetDateTime;
 
+@Builder
 public record CreateEventInput(
         String name,
         EventType type,
@@ -13,4 +16,13 @@ public record CreateEventInput(
         OffsetDateTime startDate,
         OffsetDateTime endDate
 ) {
+
+    public CreateEventInput {
+        CheckNotNull.forClass(CreateEventInput.class.getName())
+                .field("name", name)
+                .field("type", type)
+                .field("organizer", organizer)
+                .field("location", location)
+                .field("startDate", startDate);
+    }
 }
