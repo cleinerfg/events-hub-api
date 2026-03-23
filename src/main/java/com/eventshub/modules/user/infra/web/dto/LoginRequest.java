b625791dc696d7e38706762069012c510bf628db
cleinerfg <149598883+cleinerfg@users.noreply.github.com>
2026-03-22 22:20:32 -0300

    feat(user): add validation annotations to LoginRequest

--- src/main/java/com/eventshub/modules/user/infra/web/dto/LoginRequest.java
+++ src/main/java/com/eventshub/modules/user/infra/web/dto/LoginRequest.java
@@ -1,7 +1,18 @@
 package com.eventshub.modules.user.infra.web.dto;
 
+import com.eventshub.shared.infra.web.exception.ValidationMessage;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record LoginRequest(
+        @NotBlank(message = ValidationMessage.REQUIRED)
+        @Email(message = ValidationMessage.INVALID_EMAIL_FORMAT)
+        @Size(max = 255, message = ValidationMessage.MAX_LENGTH_255)
         String email,
+
+        @NotBlank(message = ValidationMessage.REQUIRED)
+        @Size(max = 255, message = ValidationMessage.MAX_LENGTH_255)
         String password
 ) {
 }
