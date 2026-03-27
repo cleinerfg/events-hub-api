e9a6927579c7ab5a224d19f57a4f01481a5e661a
cleinerfg <149598883+cleinerfg@users.noreply.github.com>
2026-03-27 15:56:19 -0300

    test: add ArchUnit rule to ensure domain purity

--- src/test/java/com/eventshub/ArchitectureTest.java
+++ src/test/java/com/eventshub/ArchitectureTest.java
@@ -17,19 +17,21 @@ public class ArchitectureTest {
     private static final String MODULE_CORE_SLICES = "com.eventshub.modules.(*).core..";
     private static final String SHARED_PACKAGE = "com.eventshub.shared..";
     private static final String ANY_CORE = "..core..";
+    private static final String ANY_DOMAIN = "..core.domain..";
+    private static final String ANY_APPLICATION = "..core.application..";
     private static final String ANY_INFRA = "..infra..";
 
     @ArchTest
     static final ArchRule core_must_be_pure = noClasses()
             .that().resideInAPackage(ANY_CORE)
             .should().dependOnClassesThat().resideInAPackage(ANY_INFRA)
-            .because("The core should not have access to infrastructure details");
+            .because("The core (domain + application)  should not have access to infrastructure details");
 
     @ArchTest
-    static final ArchRule shared_must_not_depend_on_business_modules = noClasses()
-            .that().resideInAPackage(SHARED_PACKAGE)
-            .should().dependOnClassesThat().resideInAPackage(MODULE_PACKAGE)
-            .because("The shared is multifunctional and should not be subject to specific business rules");
+    static final ArchRule domain_must_be_pure = noClasses()
+            .that().resideInAPackage(ANY_DOMAIN)
+            .should().dependOnClassesThat().resideInAPackage(ANY_APPLICATION)
+            .because("The domain is the core of the business and should be independent of application flows");
 
     @ArchTest
     static final ArchRule core_modules_must_be_isolated = slices()
@@ -45,4 +47,10 @@ public class ArchitectureTest {
             .matching(MODULE_CORE_SLICES)
             .should().notDependOnEachOther()
             .because("Core must be completely isolated from other modules including their core");
+
+    @ArchTest
+    static final ArchRule shared_must_not_depend_on_business_modules = noClasses()
+            .that().resideInAPackage(SHARED_PACKAGE)
+            .should().dependOnClassesThat().resideInAPackage(MODULE_PACKAGE)
+            .because("The shared is multifunctional and should not be subject to specific business rules");
 }
