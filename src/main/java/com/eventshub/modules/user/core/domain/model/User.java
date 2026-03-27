42dedcd1fee37634077aee2f72fa96992938e7bd
cleinerfg <149598883+cleinerfg@users.noreply.github.com>
2026-03-27 15:59:16 -0300

    refactor(user): implement domain validations in User model

--- src/main/java/com/eventshub/modules/user/core/domain/model/User.java
+++ src/main/java/com/eventshub/modules/user/core/domain/model/User.java
@@ -1,5 +1,6 @@
 package com.eventshub.modules.user.core.domain.model;
 
+import com.eventshub.modules.user.core.domain.exception.InvalidEmailException;
 import com.eventshub.modules.user.core.domain.model.input.CreateUserInput;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,11 +23,43 @@ public class User {
             UUID externalId,
             String passwordHash
     ) {
+
+        validate(externalId, input.name(), input.email(), passwordHash);
+
         return User.builder()
                 .externalId(externalId)
                 .name(input.name())
                 .email(input.email().toLowerCase())
                 .passwordHash(passwordHash)
                 .build();
+    }
+
+    private static void validate(
+            UUID id,
+            String name,
+            String email,
+            String passwordHash
+    ) {
+
+        if (id == null) {
+            throw new IllegalArgumentException("User id cannot be null");
+        }
+
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("User name cannot be blank");
+        }
+
+        if (passwordHash == null || passwordHash.isBlank()) {
+            throw new IllegalArgumentException("Password hash cannot be blank");
+        }
+
+        if (email == null || !isValidEmail(email)) {
+            throw new InvalidEmailException(email);
+        }
+    }
+
+    private static boolean isValidEmail(String email) {
+        boolean isLastChar = email.endsWith("@");
+        return email.contains("@") && !isLastChar;
     }
 }
