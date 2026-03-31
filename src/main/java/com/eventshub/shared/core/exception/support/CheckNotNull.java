22dc79b212726314b6a9deb57d5ad106a3a90331
cleinerfg <149598883+cleinerfg@users.noreply.github.com>
2026-03-31 17:13:25 -0300

    refactor(shared): use IllegalArgumentException instead of SystemIntegrityException

--- src/main/java/com/eventshub/shared/core/exception/support/CheckNotNull.java
+++ src/main/java/com/eventshub/shared/core/exception/support/CheckNotNull.java
@@ -1,6 +1,5 @@
 package com.eventshub.shared.core.exception.support;
 
-import com.eventshub.shared.core.exception.GlobalAppException;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 
@@ -15,9 +14,7 @@ public class CheckNotNull {
 
     public CheckNotNull field(String name, Object value) {
         if (value == null) {
-            throw GlobalAppException.systemIntegrity(
-                    String.format("%s is required in %s", name, className)
-            );
+            throw new IllegalArgumentException(name + " is required in " + className);
         }
         return this;
     }
