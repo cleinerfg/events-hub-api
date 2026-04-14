a6982ee2f1d6fdb45aa0b38b2d2c99571834fe94
cleinerfg <149598883+cleinerfg@users.noreply.github.com>
2026-04-14 15:12:26 -0300

    feat(event): create ParticipantsEventResponse and update EventWebMapper

--- src/main/java/com/eventshub/modules/event/infra/web/EventWebMapper.java
+++ src/main/java/com/eventshub/modules/event/infra/web/EventWebMapper.java
@@ -3,14 +3,13 @@ package com.eventshub.modules.event.infra.web;
 import com.eventshub.modules.event.core.application.usecase.command.CreateEventCommand;
 import com.eventshub.modules.event.core.application.usecase.command.UpdateEventCommand;
 import com.eventshub.modules.event.core.application.usecase.query.SearchEventQuery;
+import com.eventshub.modules.event.core.domain.dto.ParticipantEvent;
 import com.eventshub.modules.event.core.domain.model.Event;
-import com.eventshub.modules.event.infra.web.dto.CreateEventRequest;
-import com.eventshub.modules.event.infra.web.dto.EventResponse;
-import com.eventshub.modules.event.infra.web.dto.SearchEventRequest;
-import com.eventshub.modules.event.infra.web.dto.UpdateEventRequest;
+import com.eventshub.modules.event.infra.web.dto.*;
 import com.eventshub.shared.core.support.StringSanitizer;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.UUID;
 
 @Component
@@ -27,6 +26,15 @@ public class EventWebMapper {
                 .location(event.getLocation())
                 .startDate(event.getStartDate())
                 .endDate(event.getEndDate())
+                .build();
+    }
+
+    public ParticipantsEventResponse toParticipantsResponse(
+            UUID id, List<ParticipantEvent> participantsEvent
+    ) {
+        return ParticipantsEventResponse.builder()
+                .eventId(id)
+                .participants(participantsEvent)
                 .build();
     }
 
